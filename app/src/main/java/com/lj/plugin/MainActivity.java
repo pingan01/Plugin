ca141d54b004afcb07ac4c1de60c9e3a87d3edf2
pingan01 <1804866081@qq.com>
2019-03-20 15:19:24 +0800

    主APP界面

--- app/src/main/java/com/lj/plugin/MainActivity.java
+++ app/src/main/java/com/lj/plugin/MainActivity.java
@@ -2,6 +2,7 @@ package com.lj.plugin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //加载插件APK
+            }
+        });
+
+        findViewById(R.id.btn2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //跳转插件APK
+            }
+        });
     }
 }
